11917c8408e334c3912baccbe33ae1a2da313cba
Arjun G <151025764+Arjun-G22@users.noreply.github.com>
2024-01-03 12:47:26 +0530

    Update fetchData.jsx

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -3,7 +3,7 @@ export const exerciseOptions = {
     url: 'https://exercisedb.p.rapidapi.com/exercises',
     params: { limit: '1324' },
     headers: {
-        'X-RapidAPI-Key': '2c768e8d75msh0c5421da40a8e4bp1ba73ejsn5060ee188b3e',
+        'X-RapidAPI-Key': RAPID_API_KEY,
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
 };
